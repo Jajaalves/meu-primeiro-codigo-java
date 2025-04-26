0a54faf02618b52cf01f206f51a46bb9d5eb738a
Jajaalves <jarjaalves@gmail.com>
2025-04-26 09:57:14 -0300

    Add files via upload
    esse é meu arquivo sobre classe e eu também decidir colocar algumas variáveis  que aprendir

---
+++ MinhaClass.java
@@ -0,0 +1,21 @@
+public class MinhaClass{
+public static void main(String[] args){
+
+        // Atributos da classe InnerMinhaClass
+        int numero1 = 10;
+        int numero2 = 20;
+        int numero3 = 30; 
+        int resultado = numero1 + numero2 + numero3; // soma dos atributos
+        System.out.println("Resultado: " + resultado); // imprime o resultado da soma
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+   //e toda variavel tem que começar com letra minúscula
+   //variavel só aceita dois tipos de simbolos o ($ ou -) nunca pode começar com numero
+   //int 1numero = 10; //não pode começar com numero ou simbolo especial
+   //int @numero = 10; //não pode começar com numero ou simbolo especial
+} 
+    
+    
+}
